8f3efd7ec24445cc5649f09624594e3825a00354
Daniel Ukaji <dannyukaji@gmail.com>
2023-02-06 14:20:45 +0100

    New Commit

--- app/page.tsx
+++ app/page.tsx
@@ -5,7 +5,9 @@ import fetchNews from '../lib/fetchNews'
 async function HomePage() {
   
   //fetch the news data
-  //const news: NewsResponse = await fetchNews(categories.join(','))
+  const news: NewsResponse = await fetchNews(categories.join(','))
+
+  console.log(news)
 
   return (
     <div>
--- lib/fetchNews.ts
+++ lib/fetchNews.ts
@@ -1,4 +1,5 @@
 import { gql } from "graphql-request";
+import sortNewsByImage from "./sortNewsByImage";
 
 const fetchNews = async (
     category?: Category | string,
@@ -6,7 +7,7 @@ const fetchNews = async (
     isDynamic?: boolean
 ) => {
     // GraphQL query
-    const GET_QUERY = gql`
+    const query = gql`
         query MyQuery(
             $access_key: String!
             $categories: String!
@@ -43,8 +44,36 @@ const fetchNews = async (
     
 
     // Fetch function with Next.js 13 caching...
+    const res = await fetch('https://wakefield.stepzen.net/api/doltish-labradoodle/__graphql', {
+        method: 'POST',
+        cache: isDynamic ? "no-cache" : "default",
+        next: isDynamic ? { revalidate: 0 } : { revalidate: 20 },
+        headers: {
+            "Content-Type": "application/json",
+            Authorization: `Apikey ${process.env.STEPZEN_API_KEY}`,
+        },
+        body: JSON.stringify({
+            query,
+            variables: {
+                access_key: process.env.MEDIASTACK_API_KEY,
+                categories: category,
+                keywords: keywords,
+            },
+        }),
+    });
+
+    console.log(
+        "LOADING NEW DATA FROM API for category >>> ",
+        category,
+        keywords
+    );
+
+    const newsResponse = await res.json();
 
     //Sort function by images vs not images present
+    const news = sortNewsByImage(newsResponse.data.myQuery);
+
+    return news;
 
     //return res
 }
